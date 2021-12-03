8e77157f12e94673f1d28229495ac713e35bc489
Philip Dukhov <philip.dukhov@gmail.com>
2021-12-03 15:08:48 +0700

    example improved

--- src/Application.kt
+++ src/Application.kt
@@ -6,10 +6,7 @@ import com.squareup.sqldelight.sqlite.driver.asJdbcDriver
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
 import java.io.File
-import java.lang.IllegalStateException
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -18,9 +15,8 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
     val config = HikariConfig().apply {
         val dbFile = File(".database/db").absoluteFile
-        if (!dbFile.parentFile.exists()) {
-            dbFile.parentFile.mkdirs()
-        }
+        dbFile.parentFile.deleteRecursively()
+        dbFile.parentFile.mkdirs()
         jdbcUrl = "jdbc:h2:file:./.database/db;MODE=MySQL"
         username = "userName"
         password = "pass"
@@ -28,17 +24,20 @@ fun Application.module() {
     val dataSource = HikariDataSource(config)
     val driver = dataSource.asJdbcDriver()
     val transacter = SqlDriverTransacter(driver)
+
     try {
         transacter.transaction {
-            Database.Schema.create(driver)
-            throw IllegalStateException()
+            driver.execute(null, "CREATE TABLE throw_test(some Text)", 0, null)
+            throw ExpectedException()
         }
-    } catch (t: Throwable) {
+    } catch (_: ExpectedException) {
+        println("expected throw")
         transacter.transaction {
-            Database.Schema.create(driver)
+            driver.execute(null, "CREATE TABLE throw_test(some Text)", 0, null)
         }
     }
 }
 
+private class ExpectedException : Exception()
 private class SqlDriverTransacter(driver: SqlDriver) : TransacterImpl(driver)
 
